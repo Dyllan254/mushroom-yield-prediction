20b92a1539b023efcdfe772991b32ba787b1ecc2
Dyllan Macharia <machariawamae5@gmail.com>
2025-05-19 15:31:16 +0300

    Port correction

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
     import os
     import uvicorn
 
-    port = int(os.environ.get("PORT", 8000))  # required by platforms like Render
+    port = int(os.environ.get("PORT", 10000))  # required by platforms like Render
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 
