dc771ad70b7b189051bcf792be60647cb88233b1
Dyllan Macharia <machariawamae5@gmail.com>
2025-05-19 14:45:57 +0300

    Second commit

--- main.py
+++ main.py
@@ -189,9 +189,5 @@ def predict_csv(file: UploadFile = File(...)):
 def read_root():
     return {"message": "Welcome to Mushroom Yield Prediction"}
 
-# Run app with Uvicorn
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 
